87cd418a2ae66a13aaf1f9e5f44da272b29ca222
Joules <salamanca.julian@correounivalle.edu.co>
2023-08-05 19:23:57 -0500

    Se arregla bug de la galeria (ya funciona)

--- src/components/exercises/LetraAventura/GameLetras.js
+++ src/components/exercises/LetraAventura/GameLetras.js
@@ -32,6 +32,7 @@ class GameLetras extends Component {
     }
 
     this.game = new Phaser.Game(config);
+    console.log('ESTA ES LA FORMA EN LA QUE SE INICIA SETTINGS = ', {setting})
     this.game.scene.start('LetrasMenu', {setting});
     this.game.scale.on('enterfullscreen', this.handleEnterFullScreen, this);
     this.game.scale.on('leavefullscreen', this.handleLeaveFullScreen, this);
