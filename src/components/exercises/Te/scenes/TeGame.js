b88a05e874afd043fb1c9e76c3b1d9d9a6a26251
Joules17 <salamanca.julian@correounivalle.edu.co>
2024-01-20 17:20:07 -0500

    Hora del te

--- src/components/exercises/Te/scenes/TeGame.js
+++ src/components/exercises/Te/scenes/TeGame.js
@@ -21,8 +21,46 @@ export default class TeGame extends Phaser.Scene {
 
     preload() { }
 
+    builder () {
+        // config rondas 
+        this.worldSizeHeigth = 600;
+        this.worldSizeWidth = 800;
+
+        // level variables 
+        this.current_level = 1;
+        this.number_fases = 5;
+        this.tries = 3; 
+        this.number_errors = 0; 
+
+        // tablero 
+        this.current_tablero = undefined; 
+
+        // timers 
+        this.gameTimeSec = 0; 
+        this.gameTimeMin = 0;
+        this.tiempo_rondas = []; 
+        this.tiempo_por_ronda = 0; // seconds
+    }
+
     create() {
+        // builder 
+        this.builder(); 
+
+        // game 
         this.game = this.sys.game
+        const settings = this.sys.settings.data.settings; 
+        console.log(this.sys.settings.data); 
+
+        // number rounds - settings 
+        if (settings.rondas !== undefined) {
+            this.number_fases = parseInt(settings.rondas); 
+        } 
+
+        // number tries - settings
+        if (settings.tries !== undefined) {
+            this.tries = parseInt(settings.tries); 
+        }
+
         // Background --------------------------------------------------
         this.add.sprite(400, 300, 'TeGameBgImg');
 
@@ -40,16 +78,46 @@ export default class TeGame extends Phaser.Scene {
         this.center_dot.fillCircle(590, 337, 10);
 
         // Panels
+        // rounds 
         this.panel_rounds = this.add.graphics();
         this.panel_rounds.fillStyle(0xffffff, 0.5);
-        this.panel_rounds.fillRect(10, 0, 200, 50);
+        this.panel_rounds.fillRect(10, 550, 200, 50);
+
+        this.rounds_text = this.add.text(15, 560, 'RONDAS: ' + this.current_level + '/' + this.number_fases, {fontFamily: 'TROUBLE', fill: '#000000' }).setFontSize(40);
+
+        // time 
+        this.panel_time = this.add.graphics();
+        this.panel_time.fillStyle(0xffffff, 0.5);
+        this.panel_time.fillRect(215, 550, 180, 50);
+
+        this.time_text = this.add.text(220, 560, 'TIEMPO: ' +  this.gameTimeMin + ':' + this.gameTimeSec, {fontFamily: 'TROUBLE', fill: '#000000' }).setFontSize(40);
+
+        // errors 
+        this.panel_errors = this.add.graphics();
+        this.panel_errors.fillStyle(0xffffff, 0.5);
+        this.panel_errors.fillRect(400, 550, 170, 50);
+
+        this.errors_text = this.add.text(405, 560, 'ERRORES: ' + this.number_errors, {fontFamily: 'TROUBLE', fill: '#000000' }).setFontSize(40);
+
+        // tries 
+        this.panel_tries = this.add.graphics();
+        this.panel_tries.fillStyle(0xffffff, 0.5);
+        this.panel_tries.fillRect(575, 550, 180, 50);
+
+        this.tries_text = this.add.text(580, 560, 'INTENTOS: ' + this.tries, {fontFamily: 'TROUBLE', fill: '#000000' }).setFontSize(40);
+        
+        // question 
+        this.panel_question = this.add.graphics(); 
+        this.panel_question.fillStyle(0x000000, 0.5);
+        this.panel_question.fillRect(50, 20, 700, 100);
+
 
         this.clocky = new Clock({
             scene: this,
             posx: 590,
             posy: 337,
-            hour: 1,
-            min: 0
+            hour: 3,
+            min: 59
         })
 
         this.clocky.update_hands();
--- src/components/exercises/Te/sprites/Clock.js
+++ src/components/exercises/Te/sprites/Clock.js
@@ -30,43 +30,28 @@ export default class extends Phaser.GameObjects.Container {
     }
 
     update_hands() {
-        const hour_angle = (360 / 12) * (this.hour % 12) + (360 / 12) * (this.min / 60);
-        const min_angle = (360 / 60) * this.min;
-
-        const hour_length = 60;
-        const min_length = 80;
+        const hour_angle = 30 * this.hour + (this.min / 2);
+        const min_angle = 6 * this.min; 
+    
+        const hour_length = 100;
+        const min_length = 130;
+    
+        // Coordenadas para la manecilla del min
+        const min_X = this.posx + min_length * Math.sin((min_angle * Math.PI) / 180);
+        const min_Y = this.posy - min_length * Math.cos((min_angle * Math.PI) / 180);
 
         // Coordenadas para la manecilla de la hora
-        const hour_X = this.posx + hour_length * Math.cos((hour_angle * Math.PI) / 180);
-        const hour_Y = this.posy - hour_length * Math.sin((hour_angle * Math.PI) / 180);
+        const hour_X = this.posx + hour_length * Math.sin((hour_angle * Math.PI) / 180);
+        const hour_Y = this.posy - hour_length * Math.cos((hour_angle * Math.PI) / 180);
+    
 
-        // Coordenadas para la manecilla de los minutos
-        let min_X, min_Y;
-
-        if (this.min === 0) {
-            // Si los minutos son 0, dibujar una línea vertical
-            min_X = this.posx;
-            min_Y = this.posy - min_length;
-        } else {
-            // Calcular las coordenadas normales
-            min_X = this.posx + min_length * Math.cos((min_angle * Math.PI) / 180);
-            min_Y = this.posy - min_length * Math.sin((min_angle * Math.PI) / 180);
-        }
-
-        console.log('Hour: x1:', this.posx, 'y1:', this.posy, 'x2:', hour_X, 'y2:', hour_Y);
-        console.log('Min: x1:', this.posx, 'y1:', this.posy, 'x2:', min_X, 'y2:', min_Y);
-
-        // Update positions
-        this.hour_hand = new Phaser.Geom.Polygon(
-            {
-                stroke: 0x000000,
-                lineWidth: 100,
-                points: [this.posx, this.posy, hour_X, hour_Y]
-            });
-        this.min_hand = new Phaser.Geom.Polygon({
-            stroke: 0x000000,
-            lineWidth: 100,
-            points: [this.posx, this.posy, min_X, min_Y]
-        });
+        //console.log('Hour: x1:', this.posx, 'y1:', this.posy, 'x2:', hour_X, 'y2:', hour_Y);
+        //console.log('Min: x1:', this.posx, 'y1:', this.posy, 'x2:', min_X, 'y2:', min_Y);
+    
+    
+        this.hour_hand = this.scene.add.line(0, 0, this.posx, this.posy, hour_X, hour_Y, 0x000000).setOrigin(0);
+        this.hour_hand.setLineWidth(5).setDepth(3);
+        this.min_hand = this.scene.add.line(0, 0, this.posx, this.posy, min_X, min_Y, 0x0000ff).setOrigin(0);
+        this.min_hand.setLineWidth(3);
     }
 }
